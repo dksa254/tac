a42513272078c7ddc9393c7ad19559e67cf6be02
dksa254 <73845033+dksa254@users.noreply.github.com>
2020-12-21 11:35:30 +0100

    Create filtering.py
    TP4

---
+++ TP4/filtering.py
@@ -0,0 +1,47 @@
+"""Filter out stopwords for word cloud"""
+
+import sys
+import nltk
+try:
+    from nltk.corpus import stopwords
+except LookupError:
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+
+sw = stopwords.words("french")
+sw += ["les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont", "tout",
+       "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc", "cet", "sous",
+       "celle", "entre", "encore", "toutes", "pendant", "moins", "dire", "cela", "non",
+       "faut", "trois", "aussi", "dit", "avoir", "doit", "contre", "depuis", "autres",
+       "van", "het", "autre", "jusqu", "ville", "trouve","mot","peu","tant","chaque",
+       "cependant", "assez","peu","beaucoup","car","trop","vient","très","alors","établir",
+       "telle","déjà","chose","part", "quant", "le","la","des","un","une", "madame", "monsieur",
+       "messieurs","mesdames","attendu","président","échevin","Monsieur le bourgmestre",
+       "van het", "Collège van Burgemeester","approbation", "Burgemeester",
+       "voorbehoud van goedkeuring door de Provinciegouverneur", "suite", "motif","vient", "agit", "bruxelles"]
+sw = set(sw)
+
+
+def filtering( folder=None):
+    if folder is None:
+        input_path = f"allyear.txt"
+        output_path = f"allyear_keywords.txt"
+    else:
+        input_path = f"{folder}/allyear.txt"
+        output_path = f"{folder}/allyear_keywords.txt"
+    output = open(output_path, "w", encoding='utf-8')
+    with open(input_path, encoding='utf-8') as f:
+        text = f.read()
+        words = nltk.wordpunct_tokenize(text)
+        kept = [w.lower() for w in words if len(
+            w) > 2 and w.isalpha() and w.lower() not in sw]
+        kept_string = " ".join(kept)
+        output.write(kept_string)
+    return f'Output has been written in {output_path}!'
+
+
+if __name__ == '__main__':
+    data_path = sys.argv[1]
+    chosen_year = sys.argv[2]
+    filtering(data_path, chosen_year)
+    
